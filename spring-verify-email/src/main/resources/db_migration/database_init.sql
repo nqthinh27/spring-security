b3c723ea97309ac32d8e9d5f4067718cc40e7767
nqthinh27 <thinhnb2712@gmail.com>
2023-06-14 17:44:06 +0700

    update database_init.sql file

--- spring-verify-email/src/main/resources/db_migration/database_init.sql
+++ spring-verify-email/src/main/resources/db_migration/database_init.sql
@@ -1,28 +1,32 @@
 create database verify_email_app;
 
-CREATE TABLE `verify_email_app`.`user` (
-  `id` INT NOT NULL,
-  `user_name` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(100) NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `activated` BOOLEAN NULL DEFAULT false,
-  `role` TINYINT(1) NULL DEFAULT 1,
-  PRIMARY KEY (`id`));
+create table role
+(
+  id tinyint not null primary key,
+  role varchar(45) null
+);
 
-CREATE TABLE `verify_email_app`.`role` (
-  `id` int NOT NULL,
-  `role` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+create table user
+(
+  id int auto_increment primary key,
+  user_name varchar(100) not null,
+  password varchar(255) not null,
+  name varchar(100) null,
+  email varchar(45) not null,
+  activated tinyint(1) default 0 null,
+  role tinyint(1) default 1 null,
+  constraint fk_user_role foreign key (role) references role (id)
+);
+
+create table activation_key
+(
+  id int auto_increment primary key,
+  user_id int null,
+  active_key varchar(255) null,
+  expired_at datetime null,
+  constraint fk_activation_user
+    foreign key (user_id) references user (id)
 );
 
 INSERT INTO `verify_email_app`.`role` (`id`, `role`) VALUES ('1', 'ROLE_USER');
 INSERT INTO `verify_email_app`.`role` (`id`, `role`) VALUES ('2', 'ROLE_ADMIN');
-
-CREATE TABLE `verify_email_app`.`activation_key` (
-  `id` INT NOT NULL auto_increment,
-  `user_id` INT NULL,
-  `key` VARCHAR(255) NULL,
-  constraint fk_activation_user foreign key (user_id) references user(id),
-  PRIMARY KEY (`id`));
-
